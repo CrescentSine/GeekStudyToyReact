f9753f5607a2febcdc5cc459afd1b9a0f2733c74
Sine Titan <565022817@qq.com>
2020-09-09 03:59:21 +0800

    toy react third class

--- toy-react.js
+++ toy-react.js
@@ -4,45 +4,104 @@ function notObject(val) {
     return typeof val !== 'object' || val === null;
 }
 
+/** @param {Range} range @param {Node} node */
+function replaceContent(range, node) {
+    range.insertNode(node);
+    range.setStartAfter(node);
+    range.deleteContents();
+
+    range.setStartBefore(node);
+    range.setEndAfter(node);
+}
+
+/** @typedef { ElementWrapper | TextWrapper | Fragment } VDOMTYPE */
+
 export class Component {
     constructor() {
         this.props = Object.create(null);
+        /** @type {Component[]} */
         this.children = [];
-        this._range = null;
     }
+    /** @param {string} key @param {unknown} value */
     setAttribute(key, value) {
         this.props[key] = value;
     }
+    /** @param {Component} comp */
     appendChild(comp) {
         this.children.push(comp)
     }
+    /** @type {VDOMTYPE} */
     get vdom() {
         return this.render().vdom;
     }
-    get vchildren() {
-        return this.children.map(child => child.vdom);
-    }
-    /** @param {Range} dom_range */
-    [RENDER_TO_DOM](dom_range) {
-        this._range = dom_range;
-        this.render()[RENDER_TO_DOM](dom_range);
-    }
-    rerender() {
-        let removeRange = this._range;
-        let insertRange = removeRange.cloneRange();
-        insertRange.collapse(true);
+    update() {
+        /**
+         * @param {VDOMTYPE} oldNode 
+         * @param {VDOMTYPE} newNode 
+         */
+        let isSameNode = (oldNode, newNode) => {
+            if (oldNode.type !== newNode.type) return false;
+            if (Object.keys(oldNode.props).length !==
+                Object.keys(newNode.props).length) return false;
+            for (let key in newNode.props) {
+                if (newNode.props[key] !== oldNode.props[key]) {
+                    return false;
+                }
+            }
+            if (newNode.type === "#text") {
+                if (newNode.content !== oldNode.content) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /**
+         * @param {VDOMTYPE} oldNode 
+         * @param {VDOMTYPE} newNode 
+         */
+        let update = (oldNode, newNode) => {
+            if (!isSameNode(oldNode, newNode)) {
+                newNode[RENDER_TO_DOM](oldNode._range);
+                return;
+            }
+            if (newNode.type !== "#fragment") {
+                newNode._range = oldNode._range;
+                if (newNode.type === "#text") return;
+            }
 
-        this[RENDER_TO_DOM](insertRange);
+            let newChilds = newNode.vchildren;
+            /** @type {VDOMTYPE[]} */
+            let oldChilds = oldNode.vchildren;
 
-        removeRange.setStart(
-            insertRange.endContainer,
-            insertRange.endOffset);
-        removeRange.deleteContents();
+            if (!newChilds.length) return;
+            let tailChild = oldChilds[oldChilds.length - 1];
+            let tailRange = tailChild ? tailChild._range : oldNode._range;
+
+            for (let i = 0; i < newChilds.length; ++i) {
+                let newChild = newChilds[i];
+                let oldChild = oldChilds[i];
+                if (oldChild) {
+                    update(oldChild, newChild);
+                }
+                else {
+                    let insertRange = tailRange.cloneRange();
+                    insertRange.collapse();
+                    newChild[RENDER_TO_DOM](insertRange);
+                    tailRange = insertRange;
+                }
+            }
+            for (let i = newChilds.length; i < oldChilds.length; ++i) {
+                oldChilds[i]._range.deleteContents();
+            }
+        };
+        let vdom = this.vdom;
+        update(this._vdom, vdom);
+        this._vdom = vdom;
     }
     setState(newState) {
         if (notObject(this.state)) {
             this.state = newState;
-            this.rerender();
+            this.update();
             return;
         }
 
@@ -55,46 +114,63 @@ export class Component {
             }
         }
         merge(this.state, newState);
-        this.rerender();
+        this.update();
     }
 }
 
-export class Fragment extends Component {
+class VDOMType extends Component {
+    get vdom() {
+        return this;
+    }
+    /** @param {Range} dom_range */
+    [RENDER_TO_DOM](dom_range) {
+        this._range = dom_range;
+    }
+}
+
+class VDOMhasVChildren extends VDOMType {
+    get vdom() {
+        this.vchildren = this.children.map(child => child.vdom);
+        return this;
+    }
+    /** @param {Range} dom_range */
+    [RENDER_TO_DOM](dom_range) {
+        super[RENDER_TO_DOM](dom_range);
+        if (!this.vchildren) this.vdom;
+    }
+}
+
+export class Fragment extends VDOMhasVChildren {
     constructor() {
         super();
+        /** @type {"#fragment"} */
         this.type = "#fragment";
-        this.key;
     }
     setAttribute(key, value) {
         if (key !== "key") return;
         super.setAttribute(key, value);
     }
-    get vdom() {
-        return this;
-    }
     /** @param {Range} dom_range */
     [RENDER_TO_DOM](dom_range) {
-        this.children.forEach(c => {
-            c[RENDER_TO_DOM](dom_range);
+        super[RENDER_TO_DOM](dom_range);
+
+        this.vchildren.forEach(c => {
             dom_range = dom_range.cloneRange();
+            c[RENDER_TO_DOM](dom_range);
             dom_range.collapse();
         });
-        dom_range.detach();
     }
 }
 
-class ElementWrapper extends Component {
+class ElementWrapper extends VDOMhasVChildren {
     constructor(tagName) {
         super();
         /** @type {keyof HTMLElementTagNameMap} */
         this.type = tagName;
     }
-    get vdom() {
-        return this;
-    }
     /** @param {Range} dom_range */
     [RENDER_TO_DOM](dom_range) {
-        dom_range.deleteContents();
+        super[RENDER_TO_DOM](dom_range);
 
         let root = document.createElement(this.type);
         for (let key in this.props) {
@@ -112,31 +188,30 @@ class ElementWrapper extends Component {
             }
         }
 
-        for (let child of this.children) {
+        for (let child of this.vchildren) {
             let childRange = document.createRange();
             childRange.setStart(root, root.childNodes.length);
             childRange.setEnd(root, root.childNodes.length);
             child[RENDER_TO_DOM](childRange);
         }
 
-        dom_range.insertNode(root);
+        replaceContent(dom_range, root);
     }
 }
 
-class TextWrapper extends Component {
+class TextWrapper extends VDOMType {
     constructor(text) {
         super();
+        /** @type {"#text"} */
         this.type = "#text"
         this.content = text;
-        this.root = document.createTextNode(text);
-    }
-    get vdom() {
-        return this;
     }
     /** @param {Range} dom_range */
     [RENDER_TO_DOM](dom_range) {
-        dom_range.deleteContents();
-        dom_range.insertNode(this.root);
+        super[RENDER_TO_DOM](dom_range);
+
+        let root = document.createTextNode(this.content);
+        replaceContent(dom_range, root);
     }
 }
 
@@ -171,10 +246,15 @@ export function createElement(type, attrs, ...childs) {
     return e;
 }
 
+/** @param {Component} component @param {Node} parentElement */
 export function render(component, parentElement) {
     let range = document.createRange();
     range.setStart(parentElement, 0);
     range.setEnd(parentElement, parentElement.childNodes.length);
     range.deleteContents();
-    component[RENDER_TO_DOM](range)
+    let root = component.vdom;
+    if (!component[RENDER_TO_DOM]) {
+        component._vdom = root;
+    }
+    root[RENDER_TO_DOM](range)
 }
